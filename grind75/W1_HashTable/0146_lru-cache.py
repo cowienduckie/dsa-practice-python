e577b35395ac8c9ba1a3ab7897449cb0d83c825b
Minh Tran <minhtran.2404@outlook.com>
2026-03-19 01:16:48 +0700

    update: LRU cache readability

--- grind75/W1_HashTable/0146_lru-cache.py
+++ grind75/W1_HashTable/0146_lru-cache.py
@@ -1,138 +1,79 @@
-from typing import Tuple
-
-
 class Node:
-    def __init__(self, data: Tuple[int, int], prev_node=None, next_node=None):
-        self.data = data
-        self.next = next_node
-        self.prev = prev_node
+
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
 
 class LinkedList:
+
     def __init__(self):
-        self.head = None
+        self.front = None
         self.rear = None
 
-    def append(self, node: Node) -> Node:
-        """
-        Append a node to the linked list
-        """
-        if not self.head:
-            self.head = node
+    def append(self, node: Node) -> None:
+        # New node is a normal node
+        if self.front and self.rear:
+            self.rear.next, node.prev = node, self.rear
             self.rear = node
-            node.prev = None
-            node.next = None
+        # New node is the first node
         else:
-            node.prev = self.rear
-            node.next = None
-            self.rear.next = node
+            self.front = node
             self.rear = node
 
-        return node
-
-    def remove(self, node: Node) -> None:
-        """
-        Remove a node from the linked list
-        """
-        prev_node = node.prev
-        next_node = node.next
-
-        if prev_node and next_node:
-            # Update the pointers of the previous and next nodes
-            prev_node.next = node.next
-            next_node.prev = node.prev
-        elif prev_node:
-            # If the node is the rear node, update the rear node
-            self.rear = prev_node
+    def remove(self, node: Node) -> Node:
+        # Remove node is a normal node
+        if node.prev and node.next:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+        # Remove node is the front node (no prev)
+        elif node.next:
+            self.front = node.next
+            self.front.prev = None
+        # Remove node is the rear node (no next)
+        elif node.prev:
+            self.rear = node.prev
             self.rear.next = None
-        elif next_node:
-            # If the node is the head node, update the head node
-            self.head = next_node
-            self.head.prev = None
-        else:
-            # If the node is the only node in the linked list
-            self.head = None
-            self.rear = None
-
+        node.prev, node.next = None, None
         return node
+
+    def pop_front(self) -> Node:
+        return self.remove(self.front)
+
+    def move_to_back(self, node: Node) -> None:
+        self.append(self.remove(node))
 
 
 class LRUCache:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.num_dict = dict()
-        self.linked_list = LinkedList()
+        self.cache_dict = {}
+        self.used_list = LinkedList()
 
     def get(self, key: int) -> int:
-        if not key in self.num_dict:
+        node = self.cache_dict.get(key)
+        if node:
+            self.used_list.move_to_back(node)
+            return node.value
+        else:
             return -1
 
-        # Move the node to the rear of the linked list
-        self.linked_list.remove(self.num_dict[key])
-        self.linked_list.append(self.num_dict[key])
-
-        return self.num_dict[key].data[1]
-
     def put(self, key: int, value: int) -> None:
-        if key in self.num_dict:
-            # If the key is in the dictionary, update the value
-            self.linked_list.remove(self.num_dict[key])
-            self.linked_list.append(self.num_dict[key])
-
-            self.num_dict[key].data = (key, value)
+        # Update cache node if key exists
+        node = self.cache_dict.get(key)
+        if node:
+            node.value = value
+            self.used_list.move_to_back(node)
         else:
-            # If the key is not in the dictionary but the capacity is full, remove the head node
-            if self.capacity == 0:
-                removed_key = self.linked_list.remove(self.linked_list.head).data[0]
-                del self.num_dict[removed_key]
-            else:
-                self.capacity -= 1
+            # Otherwise, add new node
+            node = Node(key, value)
+            self.cache_dict[key] = node
+            self.used_list.append(node)
 
-            self.num_dict[key] = self.linked_list.append(Node((key, value)))
-
-
-# Test cases
-commands = [
-    "LRUCache",
-    "put",
-    "put",
-    "put",
-    "put",
-    "get",
-    "get",
-    "get",
-    "get",
-    "put",
-    "get",
-    "get",
-    "get",
-    "get",
-    "get",
-]
-values = [
-    [3],
-    [1, 1],
-    [2, 2],
-    [3, 3],
-    [4, 4],
-    [4],
-    [3],
-    [2],
-    [1],
-    [5, 5],
-    [1],
-    [2],
-    [3],
-    [4],
-    [5],
-]
-lru_cache = None
-for i, command in enumerate(commands):
-    if command == "LRUCache":
-        lru_cache = LRUCache(values[i][0])
-        print(None)
-    elif command == "put":
-        lru_cache.put(values[i][0], values[i][1])
-        print(None)
-    elif command == "get":
-        print(lru_cache.get(values[i][0]))
+            # If number of nodes is reach capacity, remove the front node of used list as LRU item
+            if len(self.cache_dict) > self.capacity:
+                removed_node = self.used_list.pop_front()
+                del self.cache_dict[removed_node.key]
