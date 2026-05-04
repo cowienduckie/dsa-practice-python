954a6cf30f8414f6824a8cc3ec8173c688e51cc6
Minh Tran <minhtran.2404@outlook.com>
2026-05-04 17:32:41 +0700

    solve: daily LeetCode 03/05/2026

--- leetcode/problems/0048_rotate-image.py
+++ leetcode/problems/0048_rotate-image.py
@@ -4,28 +4,30 @@ from typing import List
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
-        For n x n matrix, rotating the matrix by 90 degrees is equivalent to swapping the values of 4 cells:
-        (i, j) -> (j, n - 1 - i) -> (n - 1 - i, n - 1 - j) -> (n - 1 - j, i) -> (i, j)
-
-        So we only need to iterate through the top-left quarter of the matrix:
-        - For odd n, the quarters are rectangles with a single cell in the middle
-        - For even n, the quarters are squares
+        Split the original matrix into 4 quarters around the center and rotate them clockwise following below order:
+        (r, c) -> (c, n - 1 - r) -> (n - 1 - r, n - 1 - c) -> (n - 1 - c, r) -> (r, c)
         """
 
+        # Base case
         n = len(matrix)
-        rows = n // 2
-        cols = n // 2 + (1 if n % 2 else 0)
+        if n == 1:
+            return
 
-        for i in range(rows):
-            for j in range(cols):
+        # Calculate dimension sizes of 4 quarters
+        rows = n // 2
+        cols = n // 2 + (n & 1)
+
+        # Rotate 4 quarters clockwise around center of the matrix
+        for r in range(rows):
+            for c in range(cols):
                 (
-                    matrix[i][j],
-                    matrix[j][n - 1 - i],
-                    matrix[n - 1 - i][n - 1 - j],
-                    matrix[n - 1 - j][i],
+                    matrix[r][c],
+                    matrix[c][n - 1 - r],
+                    matrix[n - 1 - r][n - 1 - c],
+                    matrix[n - 1 - c][r],
                 ) = (
-                    matrix[n - 1 - j][i],
-                    matrix[i][j],
-                    matrix[j][n - 1 - i],
-                    matrix[n - 1 - i][n - 1 - j],
+                    matrix[n - 1 - c][r],
+                    matrix[r][c],
+                    matrix[c][n - 1 - r],
+                    matrix[n - 1 - r][n - 1 - c],
                 )
